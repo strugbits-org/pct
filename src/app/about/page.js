d74537a46f56146ac0c155e931dbf3ecc693a291
devmuhamamdhamza <155154337+devmuhamamdhamza@users.noreply.github.com>
2024-04-17 17:33:55 +0500

    Add About Us Page and working on about us page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,37 @@
-export default function About() {
-    return (
-      <div className="min-h-screen">
-        <h1 className="text-center text-primary text-[36px]">About Us</h1>
-      </div>
-    );
-  }
-  
+"use client";
+
+import { HeroContentBox } from "@/components/containers";
+import { GuideForm } from "@/components/forms";
+import AboutEachService from "@/components/sections/Service/AboutEachService";
+import HeroSection from "@/components/sections/HeroSection";
+
+export default function AboutUs() {
+  return (
+    <div>
+      <HeroSection bgImage="/images/services/bg-parylene.jpg">
+        <HeroContentBox
+          title={"About Us"}
+          detail={
+            "Para-Coat Technologies has been dedicated for over 10 years to providing companies with lasting protection from various industries with our certified processes and diverse set of conformal coatings fit to withstand harsh environments. We take diligence and attentiveness with every task to exceed expectations, and to give your company the highest quality service that is needed for your success."
+          }
+        />
+
+        <GuideForm
+          title={"Download Our Guide"}
+          detail={
+            "Fill the form to unlock valuable insights and expertise by downloading our guide for free."
+          }
+        />
+      </HeroSection>
+      <AboutEachService
+        title={"Our Story"}
+        subTitle={"Reliability That Lasts"}
+        detail={
+          "Para-Coat Technologies, Inc. was established in 2010. We are an ISO 9001:2015, ISO 13485:2016 and ITAR certified manufacturer service provider specializing in the application of Parylene, Acrylic, Polyurethane, Silicone, and Epoxy conformal coatings. PCT conformal coatings find wide-ranging application in medical, electronic, automotive, military & aerospace industries. We also offer Parylene equipment sales and consulting. We are committed to providing consistent, quality service at a competitive price to ensure that we meet and/or exceed our customers’ expectations."
+        }
+        buttonText={"Contact Us Now"}
+        image={"/images/about-us-story.jpg"}
+      />
+    </div>
+  );
+}
