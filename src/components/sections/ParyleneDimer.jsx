712b2e80bcb989544c09ea7f672c90dae027f5dd
devmuhamamdhamza <155154337+devmuhamamdhamza@users.noreply.github.com>
2024-04-19 16:13:42 +0500

    working on dimer page

--- src/components/sections/ParyleneDimer.jsx
+++ src/components/sections/ParyleneDimer.jsx
@@ -11,10 +11,10 @@ import { useContext } from "react";
 const ParyleneDimer = () => {
   const {
     theme: { button },
-    data: {testinmonials}
+    data: { testinmonials },
   } = useContext(DesignContext);
   return (
-    <section className="bg-white pt-[72px]">
+    <section className="bg-white pt-[72px] lg:bg-[url('/images/lines.svg')] lg:bg-contain">
       <FullWidthContent>
         <div className="flex flex-col lg:flex-row lg:gap-x-16">
           <div className="flex-1">
@@ -63,37 +63,50 @@ const ParyleneDimer = () => {
           </div>
         </div>
       </FullWidthContent>
-      <div className="pt-36 pb-12 bg-clip-path-1 text-secondary bg-black">
-        <FullWidthContent className="max-w-[1400px] flex items-center justify-between flex-col md:flex-row gap-x-44">
-          <div className="space-y-2 text-center md:text-left pt-10 pb-6 md:pt-16 md:pb-8 gap-y-4">
+      <div className="pt-20 lg:pt-36 pb-12 bg-clip-path-1 text-secondary bg-black">
+        <FullWidthContent className="max-w-[1400px] flex items-center justify-between flex-col md:flex-row md:gap-x-44 gap-y-12">
+          <div className="flex-1 max-w-[627px] space-y-2 text-center md:text-left pt-10 pb-6 md:pt-16 md:pb-8 gap-y-4">
             <h2 className="text-xs md:text-sm mb-1 tracking-widest text-red">
               TESTIMONIALS
             </h2>
             <h1 className="text-3xl lg:text-4xl">What Our Client Says</h1>
-            <p className="text-xs lg:text-[16px]">
+            <p className="text-[13px] lg:text-[16px]">
               Read testimonials from our satisfied clients, sharing their
               experiences and satisfaction with our product. Find out why they
               trust us for their needs.
             </p>
           </div>
-          <div className="">
+          <div className="flex-1 max-w-[627px] mx-auto bg-transparent">
             <Splide
               aria-label="What We Offers"
               tag="div"
-              className="max-w-[627px] bg-transparent"
               options={{
-                arrows:false
-            }}
+                arrows: false,
+                // autoplay: true,
+                // type: "loop",
+                // interval: 3000,
+                // pauseOnHover:true
+              }}
             >
               {testinmonials.map((item) => {
                 return (
                   <SplideSlide
                     key={item.id}
-                    className="flex flex-col justify-center text-center pb-12"
+                    className="w-full flex flex-col justify-center text-center pb-12 "
                   >
-                    <Image src={item.image} alt="" className="rounded-full mb-4 mx-auto" width={100} height={100} />
-                    <p className="text-secondary text-[16px]">{item.quote}</p>
-                    <h5 className="font-pop500 text-red text-lg">{item.name}</h5>
+                    <Image
+                      src={item.image}
+                      alt=""
+                      className="rounded-full mb-4 mx-auto"
+                      width={100}
+                      height={100}
+                    />
+                    <p className="text-secondary text-sm md:text-[16px] mb-3">
+                      {item.quote}
+                    </p>
+                    <h5 className="font-pop500 text-red text-lg">
+                      {item.name}
+                    </h5>
                   </SplideSlide>
                 );
               })}
@@ -101,6 +114,54 @@ const ParyleneDimer = () => {
           </div>
         </FullWidthContent>
       </div>
+      <FullWidthContent>
+        <div className="flex flex-col lg:flex-row lg:gap-x-16 pt-6">
+          <div className="flex-1">
+            <Image
+              className="w-full h-auto mb-5 max-w-[496px] mx-auto lg:max-h-none lg:mb-0 lg:w-[48vw] lg:max-w-[696px] object-cover"
+              src={"/images/dimer-type-c-single.png"}
+              alt={"parylene-dimer"}
+              width={696}
+              height={632}
+            />
+          </div>
+          <div className="flex-1 mt-auto mb-auto mx-auto text-center lg:text-left py-0 lg:py-16 max-w-[652px]  lg:mx-0">
+            <h2 className="inline h-line text-xs md:text-sm mb-1 before:bg-red before:w-[30px] before:h-[2px] pl-[36px] text-red">
+              PARYLENE DIMER{" "}
+            </h2>
+            <h1 className="mb-4 text-[28px] md:text-5xl">Type | C</h1>
+            <p className="mb-5 md:mb-8 text-[13px] md:text-[15px]">
+              The most common type of dimer, providing a useful combination of
+              barrier and dielectric properties while also having the fastest
+              deposition rate.
+            </p>
+            <ul className="inline-block text-left space-y-2 mb-5 pl-2 list-image-[url(/images/icons/list-check.svg)]">
+              <li className="">
+                <p className="-translate-y-2">
+                  Low permeability to moisture, chemicals, and corrosive gasses
+                </p>
+              </li>
+              <li className="">
+                <p className="-translate-y-2">Most Versatile</p>
+              </li>
+              <li className="">
+                <p className="-translate-y-2">
+                  Pressure Resistant
+                </p>
+              </li>
+            </ul>
+            <br />
+            <AnimateButton
+              className={`${button.red} mb-2 shadow-[1px_1px_8px_0px] shadow-red32 hover:shadow-none before:bg-primary hover:bg-primary`}
+            >
+              {"Purchase Now"}
+            </AnimateButton>
+            <p className="mb-7 md:mb-8 text-[13px] md:text-[15px]">
+              {"Contact us directly to complete your purchase."}
+            </p>
+          </div>
+        </div>
+      </FullWidthContent>
     </section>
   );
 };
