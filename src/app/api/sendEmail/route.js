217fed1fdf0595242d8e46cafad4ab87519b3d8c
devmuhamamdhamza <155154337+devmuhamamdhamza@users.noreply.github.com>
2024-04-24 17:49:36 +0500

    update emailSend api route

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -1,28 +1,33 @@
-// import { NextResponse } from "next/server";
-// // import sgMail from "@sendgrid/mail";
-// const sgMail = require("@sendgrid/mail");
+import { NextResponse } from "next/server";
+// import sgMail from "@sendgrid/mail";
+const sgMail = require("@sendgrid/mail");
 
-// export async function POST(req) {
-//   //   const body = await req.json();
-//   sgMail.setApiKey(
-//   );
-//   const msg = {
-//     to: "muhammad.hamza.strugbits@gmail.com",
-//     from: "leo.hudson210@gmail.com", // Use the email address or domain you verified above
-//     subject: "Sending with Twilio SendGrid is Fun",
-//     text: "and easy to do anywhere, even with Node.js",
-//     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-//   };
+export async function POST(req) {
+  //   const body = await req.json();
+  sgMail.setApiKey("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");
+  const msg = {
+    to: "muhammad.hamza.strugbits@gmail.com",
+    from: "leo.hudson210@gmail.com", // Use the email address or domain you verified above
+    subject: "Sending with Twilio SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
 
-//   sgMail
-//   .send(msg)
-//   .then(() => {}, error => {
-//     console.error(error);
+  sgMail.send(msg).then(
+    () => {
+    //   return NextResponse({ message: "Email send successfully" });
+    console.log("Email sent successfully");
+    },
+    (error) => {
+      console.error(error);
 
-//     if (error.response) {
-//       console.error(error.response.body)
-//     }
-//   });
+      if (error.response) {
+        console.error(error.response.body);
+        // return NextResponse({ message: error.response.body });
+      }
+    }
+  );
+}
 
 // //   ES8
 //     // (async () => {
