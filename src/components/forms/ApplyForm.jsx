fdee8cdf6787bbaf2032bfc630285025c1a6d9b1
devmuhamamdhamza <155154337+devmuhamamdhamza@users.noreply.github.com>
2024-04-25 23:24:17 +0500

    update apply form

--- src/components/forms/ApplyForm.jsx
+++ src/components/forms/ApplyForm.jsx
@@ -3,22 +3,155 @@ import { AnimateButton } from "../Buttons";
 import { cn } from "@/lib/utils";
 import React, { useContext, useState, useRef } from "react";
 import { DesignContext } from "@/context/design";
-import Modal from "../Modals/ApplyModals/ApplyModal";
 import { applicationSchema } from "@/lib/forms/downloadGuideSchema";
 import Input from "../Input";
-import { Form } from "./Form";
 
 const ApplyForm = ({ formTitle, detail, className }) => {
   const [quoteFile, setQuoteFile] = useState(null);
-  const uploadFile = useRef("");
+  const [form, updateForm] = useState({ disabled: false, message: "" });
+  
+  const resume = useRef(null)
+
   const {
     theme: { button },
-    form,
   } = useContext(DesignContext);
+
+  const getFormValues = (form) => ({
+    firstName: form.firstName.value,
+    lastName: form.lastName.value,
+    email: form.email.value,
+    phoneNumber: form.phoneNumber.value.trim(),
+    link: form.link.value.trim(),
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = getFormValues(e.target);
+    try {
+      updateForm({
+        message: "",
+        disabled: true,
+      });
+
+      applicationSchema.parse(formData);
+      let attachmentLink = null;
+
+      // Upload File
+
+      if (resume.current?.files[0]?.type === "application/pdf") {
+        const formDataImage = new FormData();
+        formDataImage.append("file", resume.current.files[0], resume.current.files[0].fileName);
+        formDataImage.append("expires", "2024-04-26T00:00:00Z");
+        formDataImage.append("autoDelete", "true");
+
+        const fileIo = await fetch("https://file.io/", {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+            Authorization: "Bearer cf0c90ad-e01e-454b-aef9-e98e6d46d2b4",
+          },
+          body: formDataImage,
+          redirect: "follow",
+        });
+
+        if (fileIo.status === 200) {
+          let data = await fileIo.json();
+          attachmentLink = data.link;
+
+          
+        } else {
+          updateForm({
+            message: "Did not fetch the details",
+            disabled: false,
+          });
+        }
+      }
+
+      // For Admin
+      const toAdminEmail = process.env.NEXT_PUBLIC_TO_ADMIN_EMAIL;
+
+      const formAdmin = {
+        email: toAdminEmail,
+        title: `New Applicatino for ${formTitle}`,
+        text: `The details of Form Application: \n ${JSON.stringify(formData)} 
+        \n ${attachmentLink ? `File Attached: ${attachmentLink}` : ""}
+        `,
+        html: `
+          The details of submission below: <br /><br /> ${Object.keys(formData)
+            .map((key) => {
+              return `<span>${key}</span>: <span>${formData[key]}</span><br />`;
+            })
+            .join("\n")} 
+          ${attachmentLink ? `<br />File Attached: ${attachmentLink}` : ""}
+          `,
+      };
+
+      // For User
+      const userForm = {
+        email: formData.email.trim(),
+        title: `We recieved your application for ${formTitle}`,
+        text: `Thank you for your submission. we received your application we will contact you soon`,
+        html: `<strong> Thank you for your submission. we received your application we will contact you soon </strong>`,
+      };
+
+      const responseAll = await Promise.all([
+        fetch("/api/downloadGuide", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formAdmin),
+        }),
+        fetch("/api/downloadGuide", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userForm),
+        }),
+      ]);
+
+      if (responseAll[0].ok && responseAll[1].ok) {
+        const data = await responseAll[0].json();
+        updateForm({
+          message: data.message,
+          disabled: false,
+        });
+
+        e.target.reset();
+        setQuoteFile(null);
+
+        setTimeout(() => {
+          updateForm({
+            message: "",
+            disabled: false,
+          });
+        }, 3000);
+      } else {
+        updateForm({
+          message: "Did not fetch the details",
+          disabled: false,
+        });
+      }
+    } catch (e) {
+      const error = JSON.parse(e);
+      if (error?.length && error[0]?.message) {
+        updateForm({
+          message: error[0].message,
+          disabled: false,
+        });
+      } else {
+        updateForm({
+          message: "Something wrong please check your fields",
+          disabled: false,
+        });
+      }
+    }
+  };
 
   const handleOpenFile = (e) => {
     e.preventDefault();
-    uploadFile.current.click();
+    resume?.current?.click();
   };
   const handleFileChnage = (e) => {
     const file = e.target.files[0];
@@ -28,13 +161,7 @@ const ApplyForm = ({ formTitle, detail, className }) => {
     setQuoteFile(null);
   };
 
-  const getFormValues = (form) => ({
-    firstName: form.firstName.value,
-    lastName: form.lastName.value,
-    email: form.email.value,
-    phoneNumber: form.phoneNumber.value.trim(),
-    link: form.link.value.trim(),
-  });
+  
 
   return (
     <div
@@ -47,13 +174,7 @@ const ApplyForm = ({ formTitle, detail, className }) => {
         {`Apply for ${formTitle}`}
       </h1>
 
-      <Form
-        formSchema={applicationSchema}
-        getFormValues={getFormValues}
-        url="/api/downloadGuide"
-        subject={`Application Form for ${formTitle} Recieved`}
-        subjectForAdmin={`Submission of New Application for ${formTitle}`}
-      >
+      <form onSubmit={handleSubmit}>
         <div className="grid gap-3 md:gap-6 mb-6 grid-cols-2">
           <Input
             type="text"
@@ -115,20 +236,21 @@ const ApplyForm = ({ formTitle, detail, className }) => {
             boxClassName={"col-span-2"}
           />
 
-          {/* <div className="mb-4 col-span-2">
-            <Input
-              ref={uploadFile}
+          <div className="mb-4 col-span-2 text-left">
+            <label className="ml-0 mr-auto" htmlFor="uploadFile">Resume Upload</label>
+            <input
+              ref={resume}
               type="file"
               id="uploadFile"
               name="uploadFile"
-              required
+              accept="application/pdf"
               labelColor="text-primary"
               label="Resume Upload"
               className="w-0 h-0 hidden"
               onChange={handleFileChnage}
             />
             <button
-              className="w-full ring-1 ring-red rounded-sm text-red flex justify-center items-center gap-4 mb-2 py-3 px-5"
+              className="w-full ring-1 mt-1 ring-red rounded-sm text-red flex justify-center items-center gap-4 mb-2 py-3 px-5"
               onClick={handleOpenFile}
             >
               Upload Resume
@@ -143,20 +265,20 @@ const ApplyForm = ({ formTitle, detail, className }) => {
                 ? quoteFile.name
                 : "Upload supported file (Max 15MB)"}
             </small>
-          </div> */}
+          </div>
         </div>
 
         <AnimateButton
           className={`${button.red} w-full before:bg-primary hover:bg-primary`}
           disabled={form.disabled}
         >
-          Apply
+          {form.disabled ? "Applying..." : "Apply"}
         </AnimateButton>
 
         {form?.message && (
           <p className="text-primary w-full text-center my-2">{form.message}</p>
         )}
-      </Form>
+      </form>
     </div>
   );
 };
