4096b937e4cd323162b7eeee8220ab86ccc82189
devmuhamamdhamza <155154337+devmuhamamdhamza@users.noreply.github.com>
2024-04-25 06:32:19 +0500

    add google-analytics (Hamza profile we will change it later)

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header, Footer } from "@/components";
 import { DesignProvider } from "@/context/design";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,6 +13,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KT50VLEY6Z"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-KT50VLEY6Z');
+          `}
+        </Script>
+      </head>
       <body>
         <DesignProvider>
           <Header />
